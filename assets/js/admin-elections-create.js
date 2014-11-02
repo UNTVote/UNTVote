7b94256e2a9e7964d840ff20d88de33888c5f743
Keith Wickramasekara <keith.wi@live.com>
2014-11-02 10:15:45 -0600

    Minor improvements to create-election page

--- assets/js/admin-elections-create.js
+++ assets/js/admin-elections-create.js
@@ -2,6 +2,7 @@ $(function () {
   var nameTextBox = $('#electionName');
   var panelLabel = $('#panelTitle');
   
+  // Change panel title as the user types in a name
   nameTextBox.on('keyup', function () {
     if (nameTextBox.val().length <= 0) { panelLabel.html("Untitled election"); }
     else { panelLabel.html(nameTextBox.val()); }
@@ -10,14 +11,25 @@ $(function () {
 
 $(document).ready(function (){
   
-  $('#startDate, #endDate').datepicker({
-    allowPastDates: false
-  });
-  
+  // Initialize candidate list
   $('#candidateList').multiselect({
     maxHeight: 200,
     enableCaseInsensitiveFiltering: true,
     disableIfEmpty: true 
   });
+  
+  // Initialize datepickers
+  $('#startDate, #endDate').datepicker();
+  
+  // When the start date changed by user
+  $('#startDate').on('changed.fu.datepicker dateClicked.fu.datepicker', function (evt, startDate) {
+    
+    $('#endDate').datepicker('setDate', startDate);
+    $('#endDate').datepicker({
+      restricted: [{ from: '01/01/1900', to: startDate }]
+    });
+    
+  });
+
   
 });
