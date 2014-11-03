8716a0296bfaf4e8fb6196600480913c3c3ec524
Keith Wickramasekara <keith.wi@live.com>
2014-11-02 18:38:52 -0600

    Updated TOS

--- application/views/pages/terms-of-service.php
+++ application/views/pages/terms-of-service.php
@@ -1,20 +1,70 @@
         <!-- Main body content -->
         <div class="col-xs-11 col-sm-10 col-md-8 col-lg-6 panel panel-default block-center">
           <div class="panel-body">
-            <h1>Terms of Service</h1>
+            <h1>Web Site Terms and Conditions of Use</h1>
             <hr>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed consectetur eros. In hac habitasse platea dictumst. Ut id tincidunt velit, quis euismod nulla. In vitae nunc id eros eleifend scelerisque eget id nisl. Praesent arcu lacus, commodo non orci ut, consequat tristique nibh. Curabitur eget ex dignissim, faucibus sapien nec, commodo lectus. Integer porta congue ex in vulputate. Integer neque metus, pulvinar eget lacus elementum, auctor placerat dui. Proin lobortis rhoncus neque sit amet facilisis. Suspendisse efficitur lectus massa, sit amet pulvinar massa varius sit amet. Suspendisse urna velit, lacinia nec pulvinar vitae, cursus ut risus. Nullam ultricies ipsum velit, ut venenatis risus accumsan vitae.</p>
+            <br>
+            <div class="row">
+              <div class="col-xs-12">
+                <ol style="padding-left:5px;">
+                  <div class="col-xs-12 col-sm-6">
+                    <li><a href="#terms">Terms</a></li>
+                    <li><a href="#use-license">Use License</a></li>
+                    <li><a href="#disclaimer">Disclaimer</a></li>
+                    <li><a href="#limitations">Limitations</a></li>
+                    <li><a href="#revisions-and-errata">Revisions and Errata</a></li>
+                  </div>
+                  <div class="col-xs-12 col-sm-6">
+                    <li><a href="#links">Links</a></li>
+                    <li><a href="#site-terms-of-use">Site Terms of Use Modifications</a></li>
+                    <li><a href="#governing-law">Governing Law</a></li>
+                    <li><a href="#privacy-policy">Privacy Policy</a></li>
+                  </div>
+                </ol>
+              </div>
+            </div>
+            <br>
             
-            <h3>Vestibulum varius</h3>
+            <h3 id="terms">Terms</h3>
+            <p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.</p>
             
-            <p>Donec quis eros quam. Sed laoreet fringilla ipsum vitae laoreet. Aliquam mollis ipsum non mi aliquam, id hendrerit lectus dignissim. Cras euismod, enim sed egestas commodo, quam tortor mattis urna, vel dictum arcu est vel diam. Cras lacinia quam vitae nisl rhoncus, auctor consequat magna congue. Proin mauris elit, pharetra eget nisl vitae, cursus condimentum orci. Ut lobortis quis turpis vitae malesuada. Sed feugiat mattis faucibus.<br><br>
-
-Nunc suscipit dolor faucibus, gravida enim quis, convallis nulla. Suspendisse vel pharetra odio. Etiam ullamcorper mollis enim, et ultrices metus vulputate non. Duis consectetur consequat neque, sed fermentum metus vulputate id. Ut at ante vulputate, semper arcu a, pretium lectus. Maecenas tincidunt ante velit, quis fringilla felis blandit sed. Sed vitae finibus massa. Maecenas eget ligula at urna gravida ullamcorper. Duis ultricies magna vitae eleifend maximus. Integer ac felis nec orci gravida hendrerit. Pellentesque finibus ligula nunc, sed tincidunt est imperdiet eget. Nulla hendrerit neque erat, et laoreet sapien porttitor sit amet. Sed ultrices est ac feugiat egestas. Cras rutrum iaculis massa, id aliquet arcu malesuada non. Quisque pharetra fermentum arcu, id dignissim ante ultricies sit amet. Nulla lacinia, diam ut dapibus suscipit, massa sapien dignissim ligula, sed pulvinar arcu mi in nisi.</p>
+            <h3 id="use-license">Use License</h3>
+            <p>Permission is granted to temporarily download one copy of the materials (information or software) on UNTVote's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+modify or copy the materials; use the materials for any commercial purpose, or for any public display (commercial or non-commercial); attempt to decompile or reverse engineer any software contained on UNTVote's web site; remove any copyright or other proprietary notations from the materials; or transfer the materials to another person or "mirror" the materials on any other server.</p>
+              
+              <p>This license shall automatically terminate if you violate any of these restrictions and may be terminated by UNTVote at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</p>
+              
+<h3 id="disclaimer">Disclaimer</h3>
+<p>The materials on UNTVote's web site are provided "as is". UNTVote makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, UNTVote does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.</p>
             
-            <h3>Sed feugiat mattis faucibus</h3>
+            <h3 id="limitations">Limitations</h3>       
+            <p>In no event shall UNTVote or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on UNTVote's Internet site, even if UNTVote or a UNTVote authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+</p>
             
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean tincidunt quam quis pretium pretium. Morbi vitae porta sem. Cras sed euismod nulla. Phasellus aliquam semper lacus. Nunc eu sapien nec diam vestibulum ultrices non ac purus. Donec ligula nibh, dictum vitae eros nec, euismod iaculis magna. Etiam massa erat, viverra et sem ac, fermentum egestas mauris. Donec volutpat volutpat vulputate. Morbi euismod arcu vitae risus dictum hendrerit. Duis sit amet mauris vitae nisl fermentum vestibulum vitae vitae felis. Ut vel diam facilisis, tempor tellus eget, viverra justo.</p>
+            <h3 id="revisions-and-errata">Revisions and Errata</h3>       
+            <p>The materials appearing on UNTVote's web site could include technical, typographical, or photographic errors. UNTVote does not warrant that any of the materials on its web site are accurate, complete, or current. UNTVote may make changes to the materials contained on its web site at any time without notice. UNTVote does not, however, make any commitment to update the materials.
+</p>
+            <h3 id="links">Links</h3>       
+            <p>UNTVote has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by UNTVote of the site. Use of any such linked web site is at the user's own risk.</p>
+            
+            <h3 id="site-terms-of-use">Site Terms of Use Modifications</h3>       
+            <p>UNTVote may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.</p>
+            
+            <h3 id="governing-law">Governing Law</h3>       
+            <p>Any claim relating to UNTVote's web site shall be governed by the laws of the State of Texas without regard to its conflict of law provisions.</p>
+            
+            <h3 id="privacy-policy">Privacy Policy</h3>       
+            <p>Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. The following outlines our privacy policy.</p>
+            <ul style="padding-left:15px;">
+              <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
+              <li>We will collect and use of personal information solely with the objective of fulfilling those purposes specified by us and for other compatible purposes, unless we obtain the consent of the individual concerned or as required by law.</li>
+              <li>We will only retain personal information as long as necessary for the fulfillment of those purposes.</li>
+              <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
+              <li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
+              <li>We will protect personal information by reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
+              <li>We will make readily available to customers information about our policies and practices relating to the management of personal information.</li>
+            </ul>
+            
           </div>
         </div> 
         <br>
