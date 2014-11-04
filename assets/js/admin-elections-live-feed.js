21e93420a7c80591247e8c989c77e8343223a56e
Keith Wickramasekara <keith.wi@live.com>
2014-11-03 20:39:31 -0600

    Added live feed page

---
+++ assets/js/admin-elections-live-feed.js
@@ -0,0 +1,29 @@
+$(window).load(function() {
+  createChart();
+});
+
+function createChart() {
+   var data = {
+    labels: ["Steve Jobs", "Jim Carey", "Jony Ive"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [45, 20, 34]
+        }
+    ]
+  };
+  
+  // This will get the first returned node in the jQuery collection
+  var ctx = $("#liveFeedChart").get(0).getContext("2d");
+
+  // Create chart
+  var liveFeedBarChart = new Chart(ctx).Bar(data, {
+    barShowStroke: false,
+    tooltipTitleFontStyle: "normal",
+    responsive: true
+  }); 
+}
