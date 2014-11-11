e7c37129da31ea0f0e8274e5051fd1aaf1ffa024
Jimmy <Jimmy090804@gmail.com>
2014-11-10 23:16:09 -0600

    Changed title name of all user pages

--- application/views/user/user_homepage.php
+++ application/views/user/user_homepage.php
@@ -1,6 +1,6 @@
         <!-- Main body content -->
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Template</h1>
+          <h1 class="page-header">Unt Vote</h1>
 
         </div>
       </div>
