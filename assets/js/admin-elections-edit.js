57d397c45e4cda507e5cdc9dc93da17a1dd7c961
Keith Wickramasekara <keith.wi@live.com>
2014-11-11 21:58:59 -0600

    Populate dates on edit election calendars with server data
    Just add data-start-date=“<?php>“ to start calendar input
    and data-end-date=“<?php>” to end calendar input. JS takes care of the
    rest

---
+++ assets/js/admin-elections-edit.js
@@ -0,0 +1,59 @@
+$(function () {
+  var nameTextBox = $('#electionName');
+  var panelLabel = $('#panelTitle');
+  
+  // Change panel title as the user types in a name
+  nameTextBox.on('keyup', function () {
+    if (nameTextBox.val().length <= 0) { panelLabel.html("Untitled election"); }
+    else { panelLabel.html(nameTextBox.val()); }
+  });
+});
+
+$(document).ready(function (){
+  var startDate = $('#startDate');
+  var startDateInput = $('#startDateInput');
+  var endDate = $('#endDate');
+  var endDateInput = $('#endDateInput');
+  
+  // Initialize category list
+  $('#categoryList').multiselect({
+    maxHeight: 200,
+    enableCaseInsensitiveFiltering: true,
+    disableIfEmpty: true 
+  });
+  
+  // Initialize candidate list
+  $('#candidateList').multiselect({
+    maxHeight: 200,
+    enableCaseInsensitiveFiltering: true,
+    disableIfEmpty: true 
+  });
+  
+  // Initialize datepickers
+  startDate.datepicker();
+  endDate.datepicker();
+  
+  // Populate dates from server data
+  startDate.datepicker('setDate', startDateInput.attr('data-start-date'));
+  endDate.datepicker('setDate', endDateInput.attr('data-end-date'));
+  
+  // When the start date changed by user
+  startDate.on('changed.fu.datepicker dateClicked.fu.datepicker', function (evt, startDateVal) {
+    
+    endDate.datepicker('setDate', startDateVal);
+    
+  });
+  
+  // If the end date is before the start date
+  endDate.on('changed.fu.datepicker dateClicked.fu.datepicker', function (evt, endDateVal) {
+    
+    if(endDateInput.val() < startDateInput.val()){
+      window.ParsleyUI.addError(endDateInput.parsley(), "invalidDate" , "Please select a date after the start date");
+    }
+    
+    if(endDateInput.val() >= startDateInput.val()){
+      window.ParsleyUI.removeError(endDateInput.parsley(), "invalidDate"); 
+    }
+  });
+    
+});
