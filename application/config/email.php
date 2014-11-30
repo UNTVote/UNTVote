6d9e85ccf41e2d6b549be2068fab4a7416e2b99e
chadsmith12@my.unt.edu <Smith.Chad12@gmail.com>
2014-11-30 14:54:10 -0600

    User Approval Emails should be fixed now
    Changed connecting to gmail over SSL

--- application/config/email.php
+++ application/config/email.php
@@ -6,7 +6,7 @@
 */
 
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
 $config['smtp_port'] = '465';
 $config['smtp_user'] = 'untvote@gmail.com';
 $config['smtp_pass'] = 'UNTVOTE2014';
